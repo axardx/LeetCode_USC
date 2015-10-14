ace05ad7ef3d5642229430418cb545042e6e1708
axardx <giool2002@gmail.com>
2015-10-13 20:02:41 -0700

    226 Invert Binary Tree
    Invert a binary tree.
    
         4
       /   \
      2     7
     / \   / \
    1   3 6   9
    
    to
    
         4
       /   \
      7     2
     / \   / \
    9   6 3   1

---
+++ 226_Invert Binary Tree.cpp
@@ -0,0 +1,29 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        
+        if ( !root )
+            return root;
+        
+        if ( root->left == NULL && root->right == NULL )
+            return root;
+        
+        TreeNode* l = invertTree(root->left);
+        TreeNode* r = invertTree(root->right);
+        
+        root->left = r;
+        root->right = l;
+        
+        return root;
+    }
+};
