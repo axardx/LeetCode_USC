3b0e16af234432888e69ca2516b64b1b95502720
axardx <giool2002@gmail.com>
2015-10-26 18:55:47 -0700

    275 H-Index II
    Follow up for H-Index: What if the citations array is sorted in
    ascending order? Could you optimize your algorithm?
    
    Hint:
    
    Expected runtime complexity is in O(log n) and the input is sorted.

---
+++ 275_H-Index II.cpp
@@ -0,0 +1,26 @@
+// refer to https://leetcode.com/discuss/63478/two-method-with-explaination
+//
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        
+        
+        int l = 0, r = citations.size()-1;
+        int length = citations.size();
+        int max = 0;
+        
+        while ( l <= r ) {
+            int mid = l + (r-l)/2;
+            if ( citations[mid] >= length-mid ) {
+                if ( length-mid > max )
+                    max = length-mid;
+                r = mid-1;
+            }
+            else
+                l = mid + 1;
+        }
+        return max;
+        
+    }
+};
